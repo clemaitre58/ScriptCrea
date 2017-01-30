1f641f41822faf7f9f6dcee7c8e692101bee390c
Cedric Lemaitre <c.lemaitre58@gmail.com>
2017-01-30 17:13:51 +0100

    swr

--- utils/etude.py
+++ utils/etude.py
@@ -1,1 +1,22 @@
-c
+import csv
+
+class EtudeFiches:
+    def __init__(self, dir_proj, existing_out_file = None):
+        self._list_project = []
+        self._dir_proj = dir_proj
+        self._existing_out_file = existing_out_file
+        
+        if (self._existing_out_file == None):
+            self._existing_out_file = self._dir_proj + '/Suivi/suivi.csv'
+            _create_empty_ofile(self._existing_out_file)
+
+
+    def check_fiche_vs_xls():
+
+    def _create_empty_ofile(self, name):
+        myfile = open(name, 'wb')
+        myfile.close()
+
+
+
+
