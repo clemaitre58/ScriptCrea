f7cdd0e7da53a3924ccd3c84ac1ad68383f6b3f6
Cedric Lemaitre <c.lemaitre58@gmail.com>
2017-01-30 16:25:50 +0100

    commit init

---
+++ utils/project.py
@@ -0,0 +1,15 @@
+class Project :
+    def __init__(self, name_projet) :
+        self._name_project = name_project
+        self._nb_heures = []
+        self.status = []
+        self.add_fiche = []
+
+    def add_trimester() :
+        #define different step
+        #voir comment on passe les données
+        #recherche du de la similarité en termes de titre
+        #ajouter liens vers les fiches T1, T2, T3, T4
+
+
+
