255c2fca20d3d7ea5bb5bbac94cd9e69dc12780e
Cedric Lemaitre <c.lemaitre58@gmail.com>
2017-01-30 18:01:30 +0100

    dev fonction init trimestre en cours

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 from utils.etude import *
 
+EtudeCrea2016 = EtudeFiches('/home/cedric/Documents/Conseil/2016/Creative',None)
+
 
--- utils/etude.py
+++ utils/etude.py
@@ -7,15 +7,30 @@ class EtudeFiches:
         self._existing_out_file = existing_out_file
         
         if (self._existing_out_file == None):
-            self._existing_out_file = self._dir_proj + '/Suivi/suivi.csv'
-            _create_empty_ofile(self._existing_out_file)
+            self._existing_out_file = self._dir_proj + '/Suivis/suivi.csv'
+            self._create_empty_ofile(self._existing_out_file)
 
 
-    def check_fiche_vs_xls():
+    # def check_fiche_vs_xls():
 
     def _create_empty_ofile(self, name):
-        myfile = open(name, 'wb')
+        with open(name, 'wb') myfile :
         myfile.close()
+
+    def _create_new_trimester(self, name_tri):
+
+        if name_tri == 'T1':
+        
+        elif name_tri == 'T2':
+
+        elif name_tri == 'T3':
+
+        elif name_tri == 'T4':
+
+        else:
+            print "Given value for name au trimester not allow \
+                    waiting T1, T2, T3 or T4"
+
 
 
 
