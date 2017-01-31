bd0fb6248250e9434747283ff9e9003e54eae2f2
Cedric Lemaitre <c.lemaitre58@gmail.com>
2017-01-31 11:37:50 +0100

    add list trimester fiches

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from utils.etude import *
 
-EtudeCrea2016 = EtudeFiches('/home/cedric/Documents/Conseil/2016/Creative',None)
+EtudeCrea2016 = EtudeFiches('/home/cedric/Documents/Conseil/Creative',None)
+EtudeCrea2016._create_new_trimester('T4', '2016')
+EtudeCrea2016.print_all_tri_fiches()
 
 
--- utils/etude.py
+++ utils/etude.py
@@ -1,37 +1,47 @@
 import csv
-
+import os
 class EtudeFiches:
     def __init__(self, dir_proj, existing_out_file = None):
         self._list_project = []
         self._dir_proj = dir_proj
         self._existing_out_file = existing_out_file
+        self._data_path = self._dir_proj + '/Data'
         
         if (self._existing_out_file == None):
-            self._existing_out_file = self._dir_proj + '/Suivis/suivi.csv'
+            self._existing_out_file = self._dir_proj + '/Suivi/suivi.csv'
             self._create_empty_ofile(self._existing_out_file)
 
 
     # def check_fiche_vs_xls():
 
     def _create_empty_ofile(self, name):
-        with open(name, 'wb') myfile :
-        myfile.close()
+        with open(name, 'wb') as fl:
+            fl.close()
 
-    def _create_new_trimester(self, name_tri):
+    def _create_new_trimester(self, name_tri, name_year):
 
-        if name_tri == 'T1':
-        
-        elif name_tri == 'T2':
-
-        elif name_tri == 'T3':
-
-        elif name_tri == 'T4':
-
+        if (name_tri == 'T1' 
+                or name_tri == 'T2'
+                or name_tri == 'T3'
+                or name_tri == 'T4'):
+            name_dir_to_list = self._data_path + '/' + name_year + '/' + name_tri + '/'
+            self._all_tri_fiches = self._list_fiche_directory(name_dir_to_list)
+            # load base_mission
         else:
-            print "Given value for name au trimester not allow \
-                    waiting T1, T2, T3 or T4"
+            print "Given value for name au trimester not allow! Waiting T1, T2, T3 or T4"
 
+    def _list_fiche_directory(sefl, dir_path):
+        list_files = []
+        for root, dirs, files in os.walk(dir_path): 
+            for nom_fichier in files:
+                list_files.append(os.path.join(root, nom_fichier))
 
+        for idx, iten in enumerate(list_files):
+            if( list_files[idx].find("base_mission") != -1):
+                del list_files[idx]
+        
+        return list_files
 
-
-
+    def print_all_tri_fiches(self):
+        for name in self._all_tri_fiches:
+            print name
